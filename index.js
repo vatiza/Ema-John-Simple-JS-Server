694ed44960e4392a0cc661d8e2ee3726597399c8
vatiza <rakibur.mac@gmail.com>
2024-05-03 04:34:38 +0600

    cart data load

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const port = process.env.PORT || 5000;
 app.use(cors());
@@ -30,13 +30,25 @@ async function run() {
       console.log(req.query);
       const page = parseInt(req.query.page) || 0;
       const limit = parseInt(req.query.limit) || 10;
-    const skip=page*limit;
-      const result = await productCollection.find().skip(skip).limit(limit).toArray();
+      const skip = page * limit;
+      const result = await productCollection
+        .find()
+        .skip(skip)
+        .limit(limit)
+        .toArray();
       res.send(result);
     });
     app.get("/totalProducts", async (req, res) => {
       const result = await productCollection.estimatedDocumentCount();
       res.send({ totalProducts: result });
+    });
+    app.post("/productsbyid", async (req, res) => {
+      const ids = req.body;
+      const objectids = ids.map((id) => new ObjectId(id));
+      const query = { _id: { $in: objectids } };
+      const result = await productCollection.find(query).toArray();
+      res.send(result);
+      console.log(ids);
     });
 
     // Send a ping to confirm a successful connection
